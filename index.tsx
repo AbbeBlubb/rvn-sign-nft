dcc69d08ee8a4b7359640e10b615bf9f8e660199
Alberto Chávez Pérez <abbe@Studio>
2023-12-17 22:06:30 +0100

    Initial React file

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return(
+        <section>
+            <h1>Sign in</h1>
+            <p>Ravencoin NFT</p>
+        </section>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
