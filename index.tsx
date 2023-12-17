d8c2b7104850d06bf1632a2d5eab763e4c6f8e1d
Alberto Chávez Pérez <abbe@Studio>
2023-12-17 22:20:19 +0100

    Prettier run on project

--- index.tsx
+++ index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-    return(
-        <section>
-            <h1>Sign in</h1>
-            <p>Ravencoin NFT</p>
-        </section>
-    )
+  return (
+    <section>
+      <h1>Sign in</h1>
+      <p>Ravencoin NFT</p>
+    </section>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
